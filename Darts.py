31fce3246de5347dbe318cf7ff512aa29035f870
daanjonees <daniellancejones@gmail.com>
2025-05-28 18:27:59 +0100

    Added loop for game to restart

--- Darts.py
+++ Darts.py
@@ -1,9 +1,12 @@
-print('Welcome to Daniels dart game!')
-
-remaining = 501
-while remaining > 0:
-    print('What score did you get?')
-    score = int(input())
-    remaining = remaining - score 
-    print(str(remaining) + ' Remaining')
-print('You have chekced out!')
+while 1:
+    print('Welcome to Daniels dart game!')
+    remaining = 501
+    while remaining > 0:
+        print('What score did you get?')
+        score = int(input())
+        remaining = remaining - score 
+        print(str(remaining) + ' Remaining')
+    print('You have checked out! Do you want to play again?')
+    keep_going = input()
+    if keep_going != "y":
+        break
