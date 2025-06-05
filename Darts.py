39a6833d302c668de5791711d60fa1d50c2b753a
daanjonees <daniellancejones@gmail.com>
2025-06-05 07:39:36 +0100

    Added function for seeing if a player can checkout along with outputs. Also included inital input for player to confirm their starting score.

--- Darts.py
+++ Darts.py
@@ -1,10 +1,17 @@
+def can_checkout(rem):
+    return bool((2 <= rem <= 170 and rem not in (169, 168, 166, 165, 163, 162, 159)))
+    
 while 1:
     print('Welcome to Daniels dart game!')
-    remaining = 501
+    print('What game would you like to play?')
+    remaining = int(input())
     while remaining > 0:
         print('What score did you get?')
         score = int(input())
         remaining = remaining - score 
+        cc = can_checkout(remaining)
+        if cc is False:
+            print("You can't checkout")
         print(str(remaining) + ' Remaining')
     print('You have checked out! If you want to play again please input "y".')
     keep_going = input()
