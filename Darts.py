94eda53aa0552bea8bba525b2052db9ee6fd90c0
daanjonees <daniellancejones@gmail.com>
2025-06-10 07:39:58 +0100

    Added spaces between while loops for easier reading. Also began fleshing out the make_player function. Looking to put the players names within a list to call on their turn.

--- Darts.py
+++ Darts.py
@@ -1,3 +1,6 @@
+def make_player(name):
+    return {'name': name}
+
 def can_checkout(rem):
     return bool((rem <= 170 and rem not in (169, 168, 166, 165, 163, 162, 159)) or rem > 180)
 
@@ -6,21 +9,33 @@ def scoring_input(scr):
 
 keep_going = 'y'
 
-while keep_going == 'y':
-    print('Welcome to Daniels dart game!')
+print('Welcome to Daniels dart game!')
+
+num_players = int(input("How many players are playing? "))
+current_player = 0
+game_players = []
+for x in range(num_players):
+    player_name = input('Player ' + str(current_player + 1) + ' name? ')
+    game_players.append(make_player(player_name))
+
+while keep_going == 'y': #Begins the game loop
     print('What game would you like to play?')
     remaining = int(input())
-    while remaining > 0:
+
+    while remaining > 0: 
         print('What score did you get?')
         score = int(input())
+
         while scoring_input(score) is False:
             print('The score entered is > 180, please input a new score')
             score = int(input())
         remaining = remaining - score 
         cc = can_checkout(remaining)
+
         while remaining == 1:
             print('You have gone bust!')
             remaining = remaining + score
+
         if cc is False:
             print("You can't checkout")
         print(str(remaining) + ' Remaining')
