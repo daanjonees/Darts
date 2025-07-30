4784b7302d97b1c0b628a4cffb555238f4776d54
daanjonees <daniellancejones@gmail.com>
2025-07-30 15:34:12 +0100

    Added Flask inside along with two templates. Increased functionality for 2 players

--- Darts.py
+++ Darts.py
@@ -1,6 +1,7 @@
-def make_player(name,starting_score):
-    return {'name': name,
-            'score': starting_score}
+class Players:
+    def __init__(self, name, score):
+     self.name = name
+     self.score = score
 
 def can_checkout(rem):
     return bool((rem <= 170 and rem not in (169, 168, 166, 165, 163, 162, 159)) or rem > 180)
@@ -16,25 +17,34 @@ def player_numbers(playnum):
 
 print('Welcome to Daniels dart game!')
 
-print('What game would you like to play?') #Defines the starting scores 
+print('What game would you like to play?') 
 remaining = int(input())
 
-num_players = int(input("How many players are playing?"))
-game_players = []
-
-while player_numbers(num_players) is False:
-    print('Please input either 1 or 2 players.')
-    num_players = int(input())
+print("How many players are playing?")
+num_players = int(input())
 
 if num_players == 1:
     singleplayer = 'y'
+    print('What is your name?')
+    P1name = input()
+    P1 = Players(P1name, remaining)
+
 elif num_players == 2:
     singleplayer = 'n'
+    print('What is the name of player 1?')
+    P1name = input()
+    P1 = Players(P1name, remaining)
+
+    print('What is the name of player 2?')
+    P2name = input()
+    P2 = Players(P2name, remaining)
+
     print('Who will be throwing first? Player 1 or 2?')
     throw_first = int(input())
+    
     while throwing_input(throw_first) is False:
-     print('Please confirm either player 1 or 2.')
-     throw_first = int(input())
+        print('Please confirm either player 1 or 2.')
+        throw_first = int(input())
     
     if throw_first == 1:
         player1_status = True
@@ -42,57 +52,89 @@ elif num_players == 2:
     elif throw_first == 2:
         player1_status = False
         player2_status = True
-        remaining = player1_starting_score
-        remaining = player2_starting_score    
 
-
-#for x in range(num_players):
-#    player_name = input('Player ' + str(x + 1) + ' name? ')
-#    game_players.append(make_player(player_name,remaining))
-
-
-#for player in game_players: #Used to see if I can call back the player name currently, no real use of the functionality. 
-#    print('Hello ' + str(player["name"] ))
 
 while singleplayer == 'y': #Begins the game loop currently this holds the main game loop for the game to begin, need to re-arrange. 
 
-    while remaining > 0: 
-        print('What score did you get?')
-        score = int(input())
-
-    while scoring_input(score) is False:
-        print('The score entered is > 180, please input a new score')
-        score = int(input())
-        remaining = remaining - score 
-        cc = can_checkout(remaining)
-
-        if remaining == 1:
-            print('You have gone bust!')
-            remaining = remaining + score
-
-        if cc is False:
-            print("You can't checkout")
-        print(str(remaining) + ' Remaining')
-        print('You have checked out! If you want to play again please input "y".')
-        keep_going = input()
-
-while singleplayer == 'n': 
-    while remaining > 0: 
+    while P1.score > 0: 
+            
+        print('Remaining score : ' + str(P1.score))
         print('What score did you get?')
         score = int(input())
 
         while scoring_input(score) is False:
             print('The score entered is > 180, please input a new score')
             score = int(input())
-        remaining = remaining - score 
-        cc = can_checkout(remaining)
 
-        if remaining == 1:
-            print('You have gone bust!')
-            remaining = remaining + score
+        P1.score = P1.score - score 
+        cc = can_checkout(P1.score)
+
+        while P1.score == 1:
+            print('You have gone bust')
+            P1.score = P1.score + score
 
         if cc is False:
-            print("You can't checkout")
-        print(str(remaining) + ' Remaining')
-    print('You have checked out! If you want to play again please input "y".')
-    keep_going = input()    
+            print('Checkout is possible:') #need to work out why this isn't working.
+    singleplayer == 'n'
+    print('You have checked out')    #need to fix an infinite loop here.
+
+
+
+while singleplayer == 'n': 
+
+    if player1_status == True:
+    
+        if P1.score > 0:         
+            print('Remaining score : ' + str(P1.score))
+            print('What score did you get?')
+            score = int(input())
+
+            if scoring_input(score) is False:
+                print('The score entered is > 180, please input a new score')
+                score = int(input())
+
+            P1.score = P1.score - score 
+            cc = can_checkout(P1.score)
+
+            if P1.score == 1:
+                print('You have gone bust')
+                P1.score = P1.score + score
+
+            if cc is False:
+                print('Checkout is possible:') #need to work out why this isn't working.
+        print('You have checked out')    #need to fix an infinite loop here.
+
+        if P1.score > 0:
+                    print("P2's turn")
+                    player1_status = False
+                    player2_status = True 
+        else: print(str(P2.name) + 'turn to throw.')         
+    
+    elif player2_status == True:
+    
+        if P2.score > 0: 
+                
+            print('Remaining score : ' + str(P2.score))
+            print('What score did you get?')
+            score = int(input())
+    
+            if scoring_input(score) is False:
+                print('The score entered is > 180, please input a new score')
+                score = int(input())
+
+            P2.score = P2.score - score 
+            cc = can_checkout(P1.score)
+
+            if P2.score == 1:
+                print('You have gone bust')
+                P2.score = P2.score + score
+
+            if cc is False:
+                print('Checkout is possible:') #need to work out why this isn't working.
+        print('You have checked out')    #need to fix an infinite loop here.
+
+        if P2.score > 0:
+                    print("P1's turn")
+                    player1_status = True
+                    player2_status = False 
+        else: print(str(P1.name) + 'turn to throw.')       
